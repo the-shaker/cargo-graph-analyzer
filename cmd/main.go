c41126ea4c0f35a651b3fb98ce5053c22fc80401
nikitayank2006 <yankovskiy.n@witly.su>
2025-11-15 06:04:37 +0300

    stage 2: basic Cargo dependency collection

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"cargo-depgraph/internal/cli"
 	"log"
+	"os"
 )
 
 func main() {
 	err := cli.RunCLI()
 	if err != nil {
 		log.Printf("error with running cli: %v", err)
+		os.Exit(1)
 	}
 }
